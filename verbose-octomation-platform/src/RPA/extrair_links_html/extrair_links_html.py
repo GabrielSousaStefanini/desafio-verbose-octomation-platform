0903f611f40124a6c7e85d193fc8e17c1439fd27
GabrielSousaStefanini <gssousa6@stefanini.com>
2025-10-01 10:17:58 -0300

    Formatando os logs

--- verbose-octomation-platform/src/RPA/extrair_links_html/extrair_links_html.py
+++ verbose-octomation-platform/src/RPA/extrair_links_html/extrair_links_html.py
@@ -6,19 +6,12 @@ if CAMINHO_SRC not in sys.path:
     sys.path.insert(0, CAMINHO_SRC)
 
 from datetime import datetime
-from json import load
-from pathlib import Path
 
 from shared.shared_config import (
     DIRETORIO_RPA,
     criar_estrutura_log,
-    gerar_contador_arquivo,
-    identificar_contador_arquivo,
-    identificar_prefixo_arquivo,
-    identificar_sufixo_arquivo,
     localizar_data_hora,
     registar_log,
-    registar_log_decorator,
 )
 
 NOME_AUTOMACAO = os.path.basename(__file__).removesuffix('.py')
@@ -59,54 +52,63 @@ registar_log(
     arquivo_log=ARQUIVO_LOG_PROJETO,
 )
 
+def ler_arquivo(caminho_arquivo):
+     with open(caminho_arquivo, "r", encoding="utf-8") as file:
+          return file.read()
 
 def extrair_links_html():
     caminho_arquivo_entrada = input("Informe o caminho do arquivo de entrada")
     caminho_arquivo_saida = input("Informe o caminho do arquivo de Saida")
     if (os.path.isfile(caminho_arquivo_entrada)):
         if(caminho_arquivo_entrada.endswith((".html", ".htm"))):
-            with open(caminho_arquivo_entrada, "r", encoding="utf-8") as file:
-                conteudo = file.read()
-                links = set()
-                inicio_tag = 'href="'
-                fim_tag = '"'
-                quantidadeLinks = 0
-                while inicio_tag in conteudo:
+            conteudo = ler_arquivo(caminho_arquivo_entrada)
+            links = set()
+            inicio_tag = 'href="'
+            fim_tag = '"'
+            quantidadeLinks = 0
+            registar_log(
+                log_level='DEBUG',
+                mensagem=f'Iniciando extração dos links.',
+                cultura='pt_BR.UTF-8',
+                nome_handler='root',
+                arquivo_log=ARQUIVO_LOG_PROJETO,
+                )
+            while inicio_tag in conteudo:
 
-                    inicio = conteudo.index(inicio_tag)+len(inicio_tag)
-                    fim = conteudo.index(fim_tag, inicio+len(inicio_tag))
-                    tag_a = conteudo[inicio:fim]
-                    quantidadeLinks+=1
-                    registar_log(
-                    log_level='Debug',
-                    mensagem=f'Link encontrando. Quantidade Atual: {quantidadeLinks}',
-                    cultura='pt_BR.UTF-8',
-                    nome_handler='root',
-                    arquivo_log=ARQUIVO_LOG_PROJETO,
-                    )
-                   
-                    if(os.path.isfile(caminho_arquivo_saida)):
-                        with open(caminho_arquivo_saida, "r", encoding="utf-8") as instancia_arquivo_saida:
-                            arquivo_de_saida = instancia_arquivo_saida.read()
-                            if tag_a not in arquivo_de_saida:
-                                        links.add(tag_a)
-                                        registar_log(
-                                        log_level='Debug',
-                                        mensagem=f'Colocando link Único',
-                                        cultura='pt_BR.UTF-8',
-                                        nome_handler='root',
-                                        arquivo_log=ARQUIVO_LOG_PROJETO,
-                                        )
-                    else: links.add(tag_a)
-                    conteudo = conteudo[fim:]  
+                inicio = conteudo.index(inicio_tag)+len(inicio_tag)
+                fim = conteudo.index(fim_tag, inicio+len(inicio_tag))
+                tag_a = conteudo[inicio:fim]
+                quantidadeLinks+=1
+                
+                
+                if(os.path.isfile(caminho_arquivo_saida)):
+                    arquivo_de_saida = ler_arquivo(caminho_arquivo_saida)
+                    if tag_a not in arquivo_de_saida:
+                        links.add(tag_a)                  
+                else: links.add(tag_a)
+
+                conteudo = conteudo[fim:]
+                registar_log(
+                log_level='DEBUG',
+                mensagem=f'Link Encontrado: {tag_a}',
+                cultura='pt_BR.UTF-8',
+                nome_handler='root',
+                arquivo_log=ARQUIVO_LOG_PROJETO,
+                )
+            registar_log(
+                log_level='DEBUG',
+                mensagem=f'Total de Links encontrandos: {quantidadeLinks}, Links Adicionados: {len(links)}',
+                cultura='pt_BR.UTF-8',
+                nome_handler='root',
+                arquivo_log=ARQUIVO_LOG_PROJETO,
+                ) 
 
             iteracaoArquivo = 'a' if os.path.isfile(caminho_arquivo_saida) else 'w'
             with open(caminho_arquivo_saida,iteracaoArquivo, encoding="utf-8") as file:
                 file.write("\n".join(links))
 
             return (f'Quantidade de Links no arquivo: {quantidadeLinks}',
-                    f'Quantidade de Links Únicos: {len(links)}',
-                    links)
+                    f'Quantidade de Links Únicos: {len(links)}')
         else:
             registar_log(
             log_level='ERROR',
@@ -124,6 +126,7 @@ def extrair_links_html():
         nome_handler='root',
         arquivo_log=ARQUIVO_LOG_PROJETO,
         )
+        return
        
 
 
